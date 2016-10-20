f6fbe0f950f5841acc7fb156b40cf8fabc33c1fa
zhoumingque <15818139876@163.com>
2016-10-20 16:47:15 +0800

    Utils

--- app/src/main/java/com/example/zhoumq/test/Utils.java
+++ app/src/main/java/com/example/zhoumq/test/Utils.java
@@ -1,6 +1,8 @@
 package com.example.zhoumq.test;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
@@ -17,6 +19,38 @@ public class Utils {
 
     public static void showSnackbar(View v, String snackbarStr){
         Snackbar.make(v,snackbarStr,Snackbar.LENGTH_LONG).show();
+    }
+
+    public enum NETWORK_TYPE{
+        WIFI,MOBILE
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        boolean isNet = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if( null != connectivityManager){
+            NetworkInfo mNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if( null != mNetworkInfo && mNetworkInfo .isConnected()){
+                if( mNetworkInfo.getState() == NetworkInfo.State.CONNECTED){
+                    isNet = true;
+                }
+            }
+        }
+        return isNet;
+    }
+
+    public static String isNetFor(Context context){
+        String isNet = "";
+        if(!isNetworkAvailable(context)) return context.getString(R.string.nonetwork);
+        ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+        if(mNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI){
+            isNet = context.getString(R.string.wifi);
+        }else if( mNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE){
+            isNet = context.getString(R.string.mobile);
+        }
+        if(App.DEBUG) System.out.println(isNet);
+        return isNet;
     }
 
 
